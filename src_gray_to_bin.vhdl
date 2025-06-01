638bd6bdea4d60da7e9f05533272e75cacd4b5cd
adh1ka <hafizhadhi27@gmail.com>
2025-06-01 16:11:57 +0700

    Update src_gray_to_bin.vhdl

--- src_gray_to_bin.vhdl
+++ src_gray_to_bin.vhdl
@@ -2,20 +2,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity src_gray_to_bin is
-    port (
-        gray : in  STD_LOGIC_VECTOR(3 downto 0);
-        bin  : out STD_LOGIC_VECTOR(3 downto 0)
-    );
+	port (
+    	gray_in: in STD_LOGIC_VECTOR(3 downto 0);
+        bin_out: out STD_LOGIC_VECTOR(3 downto 0)
+        );
 end src_gray_to_bin;
+
 architecture Behavioral of src_gray_to_bin is
 begin
-    process(gray)
-        variable bin_temp : STD_LOGIC_VECTOR(3 downto 0);
+	process(gray_in)
+    	variable bin_temp: STD_LOGIC_VECTOR(3 downto 0);
     begin
-        bin_temp(3) := gray(3);
-        bin_temp(2) := bin_temp(3) xor gray(2);
-        bin_temp(1) := bin_temp(2) xor gray(1);
-        bin_temp(0) := bin_temp(1) xor gray(0);
-        bin <= bin_temp;
+    	bin_temp(3) := gray_in(3);
+        for i in 2 downto 0 loop
+        	bin_temp(i) := bin_temp(i + 1) xor gray_in(i);
+        end loop;
+        bin_out <= bin_temp;
     end process;
 end Behavioral;
