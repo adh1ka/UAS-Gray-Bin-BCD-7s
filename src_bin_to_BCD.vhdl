53b8d7d8ab6220ecb55e0127e6015efdb12777df
adh1ka <hafizhadhi27@gmail.com>
2025-06-01 16:16:05 +0700

    Update src_bin_to_BCD.vhdl

--- src_bin_to_BCD.vhdl
+++ src_bin_to_BCD.vhdl
@@ -5,19 +5,18 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity src_bin_to_BCD is
 	port(
-			bin: in STD_LOGIC_VECTOR(3 downto 0);
-			dec: out STD_LOGIC_VECTOR(7 downto 0)
+			bin_in: in STD_LOGIC_VECTOR(3 downto 0);
+			bcd_out: out STD_LOGIC_VECTOR(7 downto 0)
 			);
 end src_bin_to_BCD;
 
 architecture Behavioral of src_bin_to_BCD is
-
 begin
-	process(bin)
+	process(bin_in)
 	variable z : STD_LOGIC_VECTOR(11 downto 0);
 	begin
 		z :=(others => '0');
-		z(3 downto 0):= bin;
+		z(3 downto 0):= bin_in;
 		
 		for i in 0 to 3 loop
 			if z(7 downto 4) > 4 then
@@ -28,11 +27,9 @@ begin
 				z(11 downto 8):= z(11 downto 8) + 3;
 			end if;
 			
-		--	z(11 downto 1):= z(10 downto 0);
-		Z:= Z(10 downto 0) & '0';
+		z:= z(10 downto 0) & '0';
 		end loop;
 		
-		dec <= std_logic_vector(z(11 downto 4));
+		bcd_out <= std_logic_vector(z(11 downto 4));
 	end process;
 end Behavioral;
-
